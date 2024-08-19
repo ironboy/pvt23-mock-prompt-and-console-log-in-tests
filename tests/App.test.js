5413be78274df489369b9717caf231320298a346
Ironboy <ironboy@users.noreply.github.com>
2024-08-19 16:51:02 +0200

    broken out mock as separate file

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,44 +1,15 @@
-import { test, expect, vi, beforeEach } from 'vitest';
+import { test, expect } from 'vitest';
+import {
+  promptQuestions,
+  consoleOutput,
+  setMockAnswers,
+  log
+} from './helpers/mockPromptAndConsoleLog.js';
 import App from '../classes/App.js';
-
-// promptQuestion = an array of questions the program calls prompt with
-// mockAnswers = an array of answers we want to give for the questions
-// consoleOutput = an array of output the program calls console.log with
-let promptQuestions, mockAnswers, consoleOutput;
-
-// since we are going to mock console.log - save the original console.log
-// so we can still use it by calling log
-let log = console.log;
-
-// beforeEach -> do this before each test
-beforeEach(() => {
-  // set promptQuestions and mockAnswers to empty arrays;
-  promptQuestions = [];
-  mockAnswers = [];
-  // mock the prompt!
-  // vi.mock replaces parts of an import
-  // and if the export is  done using 'export default'
-  // then the part we want to replace is 'default'
-  vi.mock(import('../helpers/prompt.js'), async () => {
-    return {
-      default: (question) => {
-        // remember the question the program sends to prompt
-        promptQuestions.push(question);
-        // return a mock answers to the program
-        return mockAnswers.shift();
-      }
-    }
-  });
-  // set consoleOutput to an empty array
-  consoleOutput = [];
-  // mock console.log - not using vi.mock since
-  // console.log is a global built in function
-  console.log = (...args) => consoleOutput.push(args);
-});
 
 test('The App should ask for name and age', () => {
   // the answers we want to give when the program calls prompt
-  mockAnswers = ['Anna', '35'];
+  setMockAnswers('Anna', '35');
   // create an instance of App
   let app = new App();
   // check that the program asks the correct questions when calling prompt
